e460578eb5877cc51ec0a78e4a4dd1b1e2685039
André <andrevictor50@gmail.com>
2020-12-17 16:48:41 -0300

    first commit

---
+++ functions/currying.js
@@ -0,0 +1,29 @@
+//Currying challenge (#1)
+
+function power(base) {
+  return function(exp) {
+    return Math.pow(base, exp);
+  };
+};
+
+console.log(power(2)(8));
+
+function sum(x) {
+  return function(y) {
+    return function(z) {
+      return x + y + z;
+    };
+  };
+};
+
+console.log(sum(1)(2)(3))
+
+function calculate(x) {
+  return function(y) {
+    return function(fn) {
+      return fn(x, y);
+    };
+  };
+};
+
+console.log(calculate(30)(2)((x, y) => (x / y)));
