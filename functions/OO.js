a09660c1180c64515b7b4328c1ede2c279ac8e59
André <andrevictor50@gmail.com>
2021-01-06 09:39:49 -0300

    prototyping in object orientation

--- functions/OO.js
+++ functions/OO.js
@@ -1,7 +1,7 @@
 class Pokemon {
   constructor(name, type, trainer) {
     this.name = name;
-    this.type = type;
+    this._type = type;
     this._trainer = trainer;
   }
 
@@ -14,8 +14,21 @@ class Pokemon {
   }
 }
 
+Pokemon.prototype.attack = function() {
+  console.log(`${this.name} is attacking`);
+}
+
+Object.defineProperty(Pokemon.prototype, 'type', {
+  get: function() {
+    return this._type;
+  },
+});
+
 const pokemon = new Pokemon('Charmander', 'Fire', 'Ash');
 
 pokemon.trainer = 'red';
+pokemon.attack();
 
-console.log(pokemon.trainer);
+console.log(pokemon.type);
+
+// console.log(pokemon.trainer);
--- functions/tempCodeRunnerFile.js
+++ functions/tempCodeRunnerFile.js
@@ -1,7 +1,5 @@
-waitFor(2000)
-//   .then(() => console.log('Executing promise 1...'))
-//   .then(waitFor)
-//   .then(() => console.log('Executing promise 2...'))
-//   .then(waitFor)
-//   .then(() => console.log('Executing promise 3...'))
-//   .then(waitFor)
+Object.defineProperty(Pokemon.prototype, 'type', {
+  get: function() {
+    return this._type;
+  },
+});
