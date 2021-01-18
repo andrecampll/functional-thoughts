2236930616d672a6ca85e1cf0a5b390486931895
André <andrevictor50@gmail.com>
2021-01-17 21:58:02 -0300

    Imutability #2

---
+++ concepts/imutability_2.js
@@ -0,0 +1,33 @@
+const person = {
+  name: 'John Doe',
+  height: 2.00,
+  city: 'New York',
+}
+
+// Mutable Code
+// Attribution by reference
+// const newPerson = person;
+
+// newPerson.name = 'Sam';
+// newPerson.height = 1.70;
+
+// changePerson(newPerson);
+// console.log(newPerson);
+
+//End
+
+// Imutable Mode
+
+// Passage by reference
+function changePerson(person) {
+  const newPerson = { ...person };
+
+  newPerson.name = 'Sam';
+  newPerson.height = 1.70;
+
+  return newPerson;
+}
+
+const newPerson = changePerson(person);
+
+console.log(newPerson, person);
